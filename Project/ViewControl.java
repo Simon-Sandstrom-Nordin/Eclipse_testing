d177f3b714817f73ac86914517b0a5a9ebdcc823
MA <sandstrom.simpa@gmail.com>
2023-10-04 20:17:40 +0200

    .

--- Project/ViewControl.java
+++ Project/ViewControl.java
@@ -1,10 +1,13 @@
 package Eclipse_testing.Project;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -18,6 +21,10 @@ public class ViewControl extends JFrame implements ActionListener {
 	private int counter = 0;
 	private int[] list_i = {0,0};
     private int[] list_e = {0,0};
+    private String[] black_pieces = {"black_rook", "black_knight", "black_bishop", "black_queen", "black_king", "black_pawn"};
+	private String[] white_pieces = {"white_rook", "white_knight", "white_bishop", "white_queen", "white_king", "white_pawn"};
+    private List<String> blackPiecesList = Arrays.asList(black_pieces);
+    private List<String> whitePiecesList = Arrays.asList(white_pieces);
 	
 	public ViewControl (){    
 	    this.setTitle("the Retarded Intelligent Chess Board (RICB) by Simon SN");
@@ -51,21 +58,21 @@ public class ViewControl extends JFrame implements ActionListener {
             for (int j = 0; j < 8; j++) {
             	// System.out.println(Integer.toString(i) + ":" + Integer.toString(j));
             	if (i == 0) {
-            		if (j == 0 || j == 7) {board[i][j] = new Square(null, black_rook);}
-            		if (j == 1 || j == 6) {board[i][j] = new Square(null, black_knight);}
-            		if (j == 2 || j == 5) {board[i][j] = new Square(null, black_bishop);}	
-            		if (j == 3) {board[i][j] = new Square(null, black_queen);}
-            		if (j == 4) {board[i][j] = new Square(null, black_king);}
+            		if (j == 0 || j == 7) {board[i][j] = new Square("black_rook", black_rook);}
+            		if (j == 1 || j == 6) {board[i][j] = new Square("black_knight", black_knight);}
+            		if (j == 2 || j == 5) {board[i][j] = new Square("black_bishop", black_bishop);}	
+            		if (j == 3) {board[i][j] = new Square("black_queen", black_queen);}
+            		if (j == 4) {board[i][j] = new Square("black_king", black_king);}
             	}
-            	if (i == 1) {board[i][j] = new Square(null, black_pawn);}
+            	if (i == 1) {board[i][j] = new Square("black_pawn", black_pawn);}
             	if (i == 2 || i == 3 || i == 4 || i == 5) {board[i][j] = new Square(null, null);}
-            	if (i == 6) {board[i][j] = new Square(null, white_pawn);}
+            	if (i == 6) {board[i][j] = new Square("white_pawn", white_pawn);}
             	if (i == 7) {
-            		if (j == 0 || j == 7) {board[i][j] = new Square(null, white_rook);}
-            		if (j == 1 || j == 6) {board[i][j] = new Square(null, white_knight);}
-            		if (j == 2 || j == 5) {board[i][j] = new Square(null, white_bishop);}	
-            		if (j == 3) {board[i][j] = new Square(null, white_queen);}
-            		if (j == 4) {board[i][j] = new Square(null, white_king);}
+            		if (j == 0 || j == 7) {board[i][j] = new Square("white_rook", white_rook);}
+            		if (j == 1 || j == 6) {board[i][j] = new Square("white_knight", white_knight);}
+            		if (j == 2 || j == 5) {board[i][j] = new Square("white_bishop", white_bishop);}	
+            		if (j == 3) {board[i][j] = new Square("white_queen", white_queen);}
+            		if (j == 4) {board[i][j] = new Square("white_king", white_king);}
             	}
             	
                 board[i][j].setPreferredSize(new Dimension(buttonSize, buttonSize)); // Set preferred size
@@ -105,12 +112,24 @@ public class ViewControl extends JFrame implements ActionListener {
 	        for (int j = 0; j < 8; j++) {
 	            if (e.getSource() == board[i][j]) {
 	            	if (counter == 0) {
+	            		//System.out.println(board[i][j].piece);
+	            		//System.out.println(board[i][j].piece == null);
+	            		//System.out.println(board[i][j].piece == "");
+	            		if (board[i][j].piece == null) {
+	            		} else {
 	            		list_i[0] = i;
 	            		list_i[1] = j;
+	            		board[i][j].setBackground(Color.orange);
+	            		counter++;
+	            		}
 	            	} else if (counter == 1) {
 	            		list_e[0] = i;
 	            		list_e[1] = j;
-	            		this.move(list_i[0],  list_i[1], list_e[0], list_e[1]);          		
+	            		if (isValidMove(board[list_i[0]][list_i[1]].piece, list_i[0], list_i[1], board[list_e[0]][list_e[1]].piece, list_e[0], list_e[1])) {
+		            	this.move(list_i[0],  list_i[1], list_e[0], list_e[1]);
+	            		board[list_i[0]][list_i[1]].setBackground(null);
+	            		counter++;
+	            		}
 	            	}
 	            	//System.out.println(i);
 	            	//System.out.println(j);
@@ -118,8 +137,42 @@ public class ViewControl extends JFrame implements ActionListener {
 	            }
 	        }
 	    }
-		counter++;
 		counter = counter % 2;
 	}
-	
+
+	private boolean isValidMove(String piece_initial, int x_i, int y_i, String piece_end, int x_e, int y_e) {
+		// It's not code repetition - it's code apartheid
+	    if (blackPiecesList.contains(piece_initial)) {
+	    	if (blackPiecesList.contains(piece_end)) {
+	    		return false;	// friendly fire
+	    	}
+	    	if (piece_initial == "black_king") {
+	    		if (Math.abs(x_i-x_e) > 1) {
+	    			return false;
+	    		} else if (Math.abs(y_i-y_e) > 1) {
+	    			return false;
+	    		} else if (Math.abs(x_i-x_e) == 1 && Math.abs(y_i-y_e) == 1) {
+	    			return true;
+	    		}
+	    	}
+	    	
+
+	    	} else {
+	    	if (whitePiecesList.contains(piece_end)) {
+	    		return false;	// friendly fire
+	    	}
+	    	if (piece_initial == "white_king") {
+	    		if (Math.abs(x_i-x_e) > 1) {
+	    			return false;
+	    		} else if (Math.abs(y_i-y_e) > 1) {
+	    			return false;
+	    		} else if (Math.abs(x_i-x_e) == 1 && Math.abs(y_i-y_e) == 1) {
+	    			return true;
+	    		}
+	    	}
+	    }
+	    
+	    return true;
+	}
+
 }
