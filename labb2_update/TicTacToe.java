90a296979e4cfb2acaa466b4c004ac40b7c0b050
MA <sandstrom.simpa@gmail.com>
2023-11-09 20:38:45 +0100

    a

--- labb2_update/TicTacToe.java
+++ labb2_update/TicTacToe.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-;
-
 public class TicTacToe implements Boardgame {
 	private int[][] board = new int[3][3];
 	private String message;
