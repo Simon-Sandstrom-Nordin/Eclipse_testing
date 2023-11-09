39b40c505494c57fc0124ab0fb00a38e6a25c400
MA <sandstrom.simpa@gmail.com>
2023-11-09 20:44:22 +0100

    a

--- labb2_update/Main.java
+++ labb2_update/Main.java
@@ -4,12 +4,12 @@ package Eclipse_testing.labb2_update;
 public class Main {
 
 	public static void main(String[] args) {
-		FifteenModel fifteenGame = new  FifteenModel();
-		ViewControl VCObject = new ViewControl(fifteenGame, 4);
-		// TicTacToeMock ticTacToeMockGame = new  TicTacToeMock();
-		// ViewControl VCObject = new ViewControl(ticTacToeMockGame, 3);
-		// TicTacToe ticTacToeGame = new TicTacToe();
-		// ViewControl VCObject = new ViewControl(ticTacToeGame, 3);
+		// FifteenModel fifteenGame = new  FifteenModel();
+		// ViewControl VCObject = new ViewControl(fifteenGame, 4);
+		TicTacToeMock ticTacToeMockGame = new  TicTacToeMock();
+		ViewControl VCObject = new ViewControl(ticTacToeMockGame, 3);
+		//TicTacToe ticTacToeGame = new TicTacToe();
+		//ViewControl VCObject = new ViewControl(ticTacToeGame, 3);
 		
 	}
 
--- labb2_update/TicTacToeMock.java
+++ labb2_update/TicTacToeMock.java
@@ -2,26 +2,18 @@ package Eclipse_testing.labb2_update;
 
 public class TicTacToeMock implements Boardgame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	@Override
 	public Boolean move(int x, int y) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public String getStatus(int x, int y) {
-		// TODO Auto-generated method stub
 		return "";
 	}
 
 	@Override
 	public String getMessage() {
-		// TODO Auto-generated method stub
 		return "Message";
 	}
 
